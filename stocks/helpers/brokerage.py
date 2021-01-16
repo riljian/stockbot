cae548222b4393d2339e33a1812245849733dd07
Jian-Min Lin <riljian.tw@gmail.com>
2021-01-16 17:37:17 +0800

    feat: fill missing stock description

--- stocks/helpers/brokerage.py
+++ stocks/helpers/brokerage.py
@@ -4,21 +4,33 @@ import shioaji as sj
 
 
 class Brokerage:
+    _adapter = None
 
     def __init__(self):
-        self._account = {
-            'username': os.getenv('BROKERAGE_USER'),
-            'password': os.getenv('BROKERAGE_PASSWORD'),
-        }
+        if self._adapter is None:
+            raise ValueError('should initialize adapter first')
+
+    @classmethod
+    def setup_adapter(cls, *args, **kwargs):
+        pass
+
+    @classmethod
+    def init_adapter(cls, *args, **kwargs):
+        if cls._adapter is None:
+            cls.setup_adapter(*args, **kwargs)
 
 
 class TwseBrokerage(Brokerage):
 
-    def __init__(self):
-        super().__init__()
-        self._adapter = sj.Shioaji()
-        self._adapter.login(
-            self._account['username'], self._account['password'])
+    @classmethod
+    def setup_adapter(cls, *args, **kwargs):
+        account = {
+            'username': os.getenv('BROKERAGE_USER'),
+            'password': os.getenv('BROKERAGE_PASSWORD'),
+        }
+        adapter = sj.Shioaji()
+        adapter.login(account['username'], account['password'])
+        cls._adapter = adapter
 
     def get_stock_name(self, code):
         return self._adapter.Contracts.Stocks[code].name
