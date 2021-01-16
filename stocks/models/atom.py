b29208e5f8470a391eaf30826b15dc44fb0609be
Jian-Min Lin <riljian.tw@gmail.com>
2021-01-16 16:03:34 +0800

    feat: adopt shioaji

--- stocks/models/atom.py
+++ stocks/models/atom.py
@@ -1,4 +1,12 @@
+import datetime
+from uuid import uuid4
+
+from caseconverter import pascalcase
+import pandas as pd
 from django.db import models
+
+import stocks.helpers.crawler as crawlers
+import stocks.helpers.brokerage as brokerages
 
 
 class Exchange(models.Model):
@@ -12,6 +20,28 @@ class Exchange(models.Model):
         constraints = [
             models.UniqueConstraint(fields=['code'], name='unique_exchange')
         ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        crawler_cls = getattr(crawlers, pascalcase(f'{self.code} crawler'))
+        if crawler_cls is None:
+            raise ValueError('crawler not found')
+        self._crawler = crawler_cls()
+
+        brokerage_cls = \
+            getattr(brokerages, pascalcase(f'{self.code} brokerage'))
+        if brokerage_cls is None:
+            raise ValueError('brokerage not found')
+        self._brokerage = brokerage_cls()
+
+    def get_stock_name(self, code: str) -> str:
+        # TODO: fetch from database if available
+        return self._brokerage.get_stock_name(code)
+
+    def get_daily_summary(self, date: datetime.date) -> pd.DataFrame:
+        # TODO: fetch from database if available
+        return self._crawler.get_daily_summary(date)
 
 
 class StockCategory(models.Model):
@@ -34,3 +64,12 @@ class Stock(models.Model):
             models.UniqueConstraint(
                 fields=['exchange', 'code'], name='unique_stock')
         ]
+
+    @staticmethod
+    def get_new_record(exchange: Exchange, code: str):
+        return Stock(
+            id=uuid4(),
+            exchange=exchange,
+            code=code,
+            description=exchange.get_stock_name(code)
+        )
