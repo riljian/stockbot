1c1136cbe897071f27c5c4ca57652729abfabe79
Jian-Min Lin <riljian.tw@gmail.com>
2021-01-17 08:48:41 +0800

    feat: add AESEncoder

---
+++ utils/__init__.py
@@ -0,0 +1,31 @@
+import base64
+import hashlib
+
+from Crypto.Cipher import AES
+
+
+class AESEncoder:
+
+    def __init__(self, key, iv):
+        self.key = hashlib.sha256(key.encode()).digest()
+        self.iv = iv  # pylint: disable=invalid-name
+
+    def encrypt(self, raw):
+        raw = self.pad(raw)
+        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+        return base64.b64encode(cipher.encrypt(raw.encode())).decode('utf-8')
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+        return self.rm_pad(cipher.decrypt(enc)).decode('utf-8')
+
+    @staticmethod
+    def pad(text):
+        block_size = AES.block_size
+        placeholder_len = block_size - len(text) % block_size
+        return text + (placeholder_len) * chr(placeholder_len)
+
+    @staticmethod
+    def rm_pad(text):
+        return text[:-ord(text[len(text)-1:])]
