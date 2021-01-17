44402eac8771daaef08c179c6dda607a21ad23c2
Jian-Min Lin <riljian.tw@gmail.com>
2021-01-17 16:29:26 +0800

    feat: add brokerage account

--- stocks/helpers/brokerage.py
+++ stocks/helpers/brokerage.py
@@ -1,5 +1,3 @@
-import os
-
 import shioaji as sj
 
 
@@ -24,13 +22,7 @@ class TwseBrokerage(Brokerage):
 
     @classmethod
     def setup_adapter(cls, *args, **kwargs):
-        account = {
-            'username': os.getenv('BROKERAGE_USER'),
-            'password': os.getenv('BROKERAGE_PASSWORD'),
-        }
+        account = kwargs['account']
         adapter = sj.Shioaji()
-        adapter.login(account['username'], account['password'])
+        adapter.login(account['user'], account['password'])
         cls._adapter = adapter
-
-    def get_stock_name(self, code):
-        return self._adapter.Contracts.Stocks[code].name
--- stocks/models/atom.py
+++ stocks/models/atom.py
@@ -1,3 +1,4 @@
+import os
 import datetime
 from uuid import uuid4
 
@@ -5,6 +6,7 @@ from caseconverter import pascalcase
 import pandas as pd
 from django.db import models
 
+from utils import AESEncoder
 import stocks.helpers.crawler as crawlers
 import stocks.helpers.brokerage as brokerages
 
@@ -15,6 +17,12 @@ class Exchange(models.Model):
     calendar_code = \
         models.CharField(max_length=10, null=True)  # trading-calendars 交易所代號
     description = models.CharField(max_length=255, null=True)
+    brokerage_user = models.CharField(max_length=255, null=True)  # 券商 API 帳號
+    brokerage_password = \
+        models.CharField(max_length=255, null=True)  # 券商 API 密碼
+
+    encoder = AESEncoder(os.getenv('BROKERAGE_ACCOUNT_KEY'),
+                         os.getenv('BROKERAGE_ACCOUNT_IV'))
 
     class Meta:
         constraints = [
@@ -24,31 +32,64 @@ class Exchange(models.Model):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        crawler_cls = getattr(crawlers, pascalcase(f'{self.code} crawler'))
+        self.init_crawler()
+        self._brokerage = None
+
+    @property
+    def crawler(self):
+        return self._crawler
+
+    @property
+    def brokerage(self):
+        if self._brokerage is None:
+            self.init_brokerage()
+        return self._brokerage
+
+    @staticmethod
+    def get_stock_crawler_class(code):
+        return getattr(crawlers, pascalcase(f'{code} crawler'), None)
+
+    @staticmethod
+    def get_stock_brokerage_class(code):
+        return getattr(brokerages, pascalcase(f'{code} brokerage'), None)
+
+    def init_crawler(self):
+        crawler_cls = self.get_stock_crawler_class(self.code)
         if crawler_cls is None:
             raise ValueError('crawler not found')
         self._crawler = crawler_cls()
 
-        brokerage_cls = \
-            getattr(brokerages, pascalcase(f'{self.code} brokerage'))
+    def init_brokerage(self):
+        brokerage_cls = self.get_stock_brokerage_class(self.code)
         if brokerage_cls is None:
             raise ValueError('brokerage not found')
-        brokerage_cls.init_adapter()
+        brokerage_cls.init_adapter(account=self.retrieve_brokerage_account())
         self._brokerage = brokerage_cls()
+
+    def setup_brokerage_account(self, user, password):
+        self.brokerage_user = self.encoder.encrypt(user)
+        self.brokerage_password = self.encoder.encrypt(password)
+        self.save()
+
+    def retrieve_brokerage_account(self):
+        return {
+            'user': self.encoder.decrypt(self.brokerage_user),
+            'password': self.encoder.decrypt(self.brokerage_password),
+        }
 
     def get_stock_name(self, code: str) -> str:
         stocks = Stock.objects.filter(exchange=self, code=code)
         if stocks.exists():
             stock = stocks[0]
             if stock.description is None:
-                stock.description = self._brokerage.get_stock_name(code)
+                stock.description = self.crawler.get_stock_name(code)
                 stock.save()
             return stock.description
-        return self._brokerage.get_stock_name(code)
+        return self.crawler.get_stock_name(code)
 
     def get_daily_summary(self, date: datetime.date) -> pd.DataFrame:
         # TODO: fetch from database if available
-        return self._crawler.get_daily_summary(date)
+        return self.crawler.get_daily_summary(date)
 
 
 class StockCategory(models.Model):
