7daf424c626eba4d9541f20ded75c8af5d1e0739
Jian-Min Lin <riljian.tw@gmail.com>
2021-03-04 08:44:12 +0800

    refactor: prevent flow dependency

--- stocks/helpers/analyzer.py
+++ stocks/helpers/analyzer.py
@@ -16,7 +16,7 @@ class Analyzer:
         self._exchange = exchange
         self._calendar = get_calendar(exchange.calendar_code)
 
-    def get_stocks_with_price(self, date) -> pd.DataFrame:
+    def get_stocks(self) -> pd.DataFrame:
         pass
 
     def get_day_trading_candidates(self, date) -> pd.DataFrame:
@@ -28,36 +28,43 @@ class TwseAnalyzer(Analyzer):
     def __init__(self):
         super().__init__(Exchange.objects.get(code='TWSE'))
 
-    def get_stocks_with_price(self, date) -> pd.DataFrame:
-        stocks = (DailySummary.objects
-                  .filter(stock__in=self._exchange.stocks.all(), date=date)
-                  .annotate(
-                      code=models.F('stock__code'),
-                      description=models.F('stock__description'),
-                      price=models.F('closing_price'),
-                  )
-                  .values('stock_id', 'code', 'description', 'price', 'trade_volume'))
+    def get_stocks(self) -> pd.DataFrame:
+        stocks = self._exchange.stocks.values('id', 'code', 'description')
         return pd.DataFrame.from_records(data=stocks)
 
-    def filter_by_price(self, df: pd.DataFrame, min_price=0.0, max_price=999999.0) -> pd.DataFrame:
-        return df[(df['price'] > min_price) & (df['price'] < max_price)]
+    def filter_by_price(self, df: pd.DataFrame, date, min_price=0.0, max_price=999999.0) -> pd.DataFrame:
+        stock_ids = (DailySummary.objects
+                     .filter(date=date, closing_price__gte=min_price, closing_price__lte=max_price)
+                     .values_list('stock_id', flat=True))
 
-    def filter_by_trade_volume(self, df: pd.DataFrame, min_volume=0) -> pd.DataFrame:
-        return df[df['trade_volume'] > min_volume]
+        return df[df['id'].map(lambda stock_id: stock_id in stock_ids)]
 
-    def filter_by_change_rate(self, df: pd.DataFrame, date, change_rate=0.0, days=1) -> pd.DataFrame:
+    def filter_by_trade_volume(self, df: pd.DataFrame, date, min_volume=0) -> pd.DataFrame:
+        stock_ids = (DailySummary.objects
+                     .filter(date=date, trade_volume__gte=min_volume)
+                     .values_list('stock_id', flat=True))
+
+        return df[df['id'].map(lambda stock_id: stock_id in stock_ids)]
+
+    def filter_by_change_rate(self, df: pd.DataFrame, date, min_change_rate=0.0, days=1) -> pd.DataFrame:
         base_date = \
             self._calendar.previous_close(date - pd.DateOffset(days=days - 1))
-        queryset = (DailySummary.objects
-                    .filter(stock__in=df['stock_id'].to_list(), date=base_date)
-                    .annotate(price=models.F('closing_price'))
-                    .values('stock_id', 'price'))
-        summary = pd.DataFrame.from_records(data=queryset)
-        summary.rename(columns={'price': 'base_price'}, inplace=True)
-        df = df.join(summary.set_index('stock_id'), on='stock_id')
-        df['change'] = df['price'] - df['base_price']
-        df['change_rate'] = df['change'] / df['base_price']
-        return df[df['change_rate'] > change_rate]
+
+        def get_summary_qs(d):
+            return DailySummary.objects.filter(date=d).values('stock_id', 'closing_price')
+
+        snapshot = pd.DataFrame.from_records(data=get_summary_qs(base_date))
+        changed_snapshot = pd.DataFrame.from_records(data=get_summary_qs(date))
+        summary = snapshot.join(changed_snapshot.set_index('stock_id'),
+                                rsuffix='_changed',
+                                on='stock_id')
+        change_rate_series = \
+            (summary['closing_price_changed'] - summary['closing_price']) \
+            / summary['closing_price']
+        change_rate_filter = change_rate_series > min_change_rate
+        stock_ids = summary[change_rate_filter]['stock_id'].to_list()
+
+        return df[df['id'].map(lambda stock_id: stock_id in stock_ids)]
 
     def get_day_trading_candidates(self, date) -> pd.DataFrame:
         calendar = self._calendar
@@ -66,10 +73,12 @@ class TwseAnalyzer(Analyzer):
             return DailySummary.objects.none()
 
         prev_trading_close = self._calendar.previous_close(date)
-        df = self.get_stocks_with_price(prev_trading_close)
-        df = self.filter_by_trade_volume(df, min_volume=50000000)
-        df = self.filter_by_price(df, min_price=5.0, max_price=30.0)
+        df = self.get_stocks()
+        df = self.filter_by_trade_volume(
+            df, prev_trading_close, min_volume=50000000)
+        df = self.filter_by_price(
+            df, prev_trading_close, min_price=5.0, max_price=30.0)
         df = self.filter_by_change_rate(
-            df, prev_trading_close, change_rate=0.04, days=1)
+            df, prev_trading_close, min_change_rate=0.04, days=1)
 
         return df
