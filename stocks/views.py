f5eb04b246298032c201e6051c310a835c0afd91
Jian-Min Lin <riljian.tw@gmail.com>
2021-03-06 06:44:38 +0800

    refactor: migrate view logic

--- stocks/views.py
+++ stocks/views.py
@@ -26,7 +26,30 @@ class StockViewSet(viewsets.ViewSet):
     def day_trading_candidates(self, request):
         exchange_code = request.query_params.get('exchange')
         date = pd.to_datetime(request.query_params.get('date'), utc=True)
-        _, analyzer = self.parse_exchange(exchange_code)
-        candidates = analyzer.get_day_trading_candidates(date)
+        exchange, analyzer = self.parse_exchange(exchange_code)
+
+        if date not in analyzer.calendar.opens:
+            logger.info('%s is closed on %s', self._exchange.code, date)
+            return response.Response({'data': []})
+
+        prev_trading_close = analyzer.calendar.previous_close(date)
+        df = analyzer.get_stocks()
+        volume_filter, volume_result = \
+            analyzer.get_trade_volume_filter(df,
+                                             prev_trading_close,
+                                             min_volume=50000000)
+        price_filter, price_result = \
+            analyzer.get_price_filter(df, prev_trading_close,
+                                      min_price=5.0,
+                                      max_price=30.0)
+        price_change_rate_filter, price_change_rate_result = \
+            analyzer.get_price_change_rate_filter(df, prev_trading_close,
+                                                  min_change_rate=0.04,
+                                                  days=1)
+
+        result = pd.concat([df, volume_result, price_result, price_change_rate_result],
+                           axis=1)
+        candidates = \
+            result[volume_filter & price_filter & price_change_rate_filter]
         data = candidates.where(~candidates.isnull(), None).to_dict('records')
         return response.Response({'data': data})
