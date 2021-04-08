1b977ba6c760c91240c0310bfe4c256bceca1504
Jian-Min Lin <riljian.tw@gmail.com>
2021-04-08 08:26:04 +0800

    fix: adopt operator update

--- stocks/views.py
+++ stocks/views.py
@@ -28,20 +28,20 @@ class StockViewSet(viewsets.ViewSet):
     def conservative_candidates(self, request):
         exchange_code = request.query_params.get('exchange')
         date = pd.to_datetime(request.query_params.get('date'), utc=True)
-        operator = self.parse_operator(exchange_code)
+        operator = self.parse_operator(f'conservative {exchange_code}')
 
-        candidates = operator.get_conservative_candidates(date)
+        candidates = operator.get_candidates(date)
         return response.Response({'data': candidates.mask(candidates.isnull(), None).to_dict('records')})
 
     @decorators.action(detail=False, methods=['get'])
     def day_trading_candidates(self, request):
         exchange_code = request.query_params.get('exchange')
         date = pd.to_datetime(request.query_params.get('date'), utc=True)
-        operator = self.parse_operator(exchange_code)
+        operator = self.parse_operator(f'day trade {exchange_code}')
 
         if date not in operator.analyzer.calendar.opens:
             logger.info('%s is closed on %s', exchange_code, date)
             return response.Response({'data': []})
 
-        candidates = operator.get_day_trade_candidates(date)
+        candidates = operator.get_candidates(date)
         return response.Response({'data': candidates.mask(candidates.isnull(), None).to_dict('records')})
